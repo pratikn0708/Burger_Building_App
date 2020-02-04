832d7a60a60a16008b9a1ef04cc80cabd3d3a334
unknown <pratikgarg0708@gmail.com>
2020-02-05 01:07:14 +0530

    Forms Validation Added

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from './Button.module.css';
 
-const button = (props) => (
+const button = (props) => (    
     <button
+        disabled={props.disabled}
         className={[classes.Button, classes[props.btnType]].join(' ')}
         onClick={props.clicked}>
         {props.children}
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import axios from '../../axios-order';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                let fetchOrder = [];
+                for (let key in res.data) {
+                    fetchOrder.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                };
+
+                this.setState({ loading: false, orders: fetchOrder })
+            })
+            .catch(error => {
+                this.setState({ loading: false })
+            });
+    };
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order=>(
+                    <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}
+                    />
+                ))}
             </div>
         )
     }
 };
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
